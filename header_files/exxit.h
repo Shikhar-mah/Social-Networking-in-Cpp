a2729e8eb6dea7a43fe1c118285e4f0bb5172e7b
Shikhar-mah <110622958+Shikhar-mah@users.noreply.github.com>
2024-11-12 11:33:28 +0530

    Add files via upload

---
+++ header_files/exxit.h
@@ -0,0 +1,30 @@
+// exxit.h
+// Copyright 2020, Konstantinos Thanos [Mathematician, MSc]. All rights reserved.
+//======================
+// Exit from the program
+//======================
+#ifndef EXXIT_H
+#define EXXIT_H
+
+// Header files
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+using namespace std;
+
+// Declaration of variables, defined in main.cpp
+extern string logname;
+
+int exxit()
+{
+    cout << endl
+         << "Program Termination..." << endl;
+    usleep(1000000); // Suspend execution for microsecond intervals
+    cout << "Thanks for your visit " + logname + " !" << endl;
+    cout << endl
+         << endl;
+    exit(0);
+    return 0;
+}
+
+#endif // EXXIT_H
